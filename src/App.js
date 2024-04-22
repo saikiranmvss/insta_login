9d3f95ac28e932a348d5debc65239066303871c0
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2024-04-22 12:51:52 +0530

    asdasdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Redirect, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 function Login() {
